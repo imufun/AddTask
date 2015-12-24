f8528bbb1a3b0c71d759908ba6a380fa7e994856
Imran <imufun1@gmail.com>
2015-12-25 02:32:35 +0600

    First

---
+++ app/src/main/java/com/android/imran/addtask/Output.java
@@ -0,0 +1,22 @@
+package com.android.imran.addtask;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+
+public class Output extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_output);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        TextView textView = (TextView) findViewById(R.id.text);
+        textView.setText(" Welcome!!\n " + getIntent().getStringExtra("Name"));
+
+//        getIntent().getStringExtra("Name",);
+    }
+
+}
